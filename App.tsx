89ad08b9acb6ed03c0a9cc12a5faf409efb39f42
Akash <codeguyakash@gmail.com>
2023-08-31 15:01:28 +0530

    create-components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 
 const App = () => {
   return (
     <View>
       <Text style={{fontSize: 50}}>Hello, world!</Text>
+      <UserData />
+      <CompanyData />
+      <Button title="Press Here"></Button>
+      <Text style={{fontSize: 50}}>Hello, world!</Text>
+      <Button title="Press Here"></Button>
     </View>
   );
 };
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 40}}>Akash!</Text>
+      <Text style={{fontSize: 40}}>Frontend Software Engineer</Text>
+    </View>
+  );
+};
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Company Name : BrainyDx Technologies</Text>
+      <Text style={{fontSize: 30}}>Since : 2010 </Text>
+      <Text style={{fontSize: 30}}>Empolyees : 500</Text>
+    </View>
+  );
+};
+
 export default App;
