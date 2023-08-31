ccc44591df52428463c76c413291b7dfebc8acec
Akash <codeguyakash@gmail.com>
2023-08-31 13:26:48 +0530

    Intial Commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 50}}>Hello, world!</Text>
+    </View>
+  );
+};
+export default App;
